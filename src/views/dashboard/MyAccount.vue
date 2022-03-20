535efae92b08d7bd0cce356935889bcbce54614e
Yannis Haismann <72089971+YannisHaismann@users.noreply.github.com>
2022-03-20 17:50:36 +0100

    feat: my account page responsive

--- src/views/dashboard/MyAccount.vue
+++ src/views/dashboard/MyAccount.vue
@@ -24,7 +24,7 @@ export default defineComponent({
         <div class="p-4 tablet:p-10">
           <div>
             <p class="text-white font-maven-medium text-12px tablet:text-20px">Private informations</p>
-            <div class="mt-1 tablet:mt-4 flex gap-4 tablet:gap-24">
+            <div class="mt-1 tablet:mt-4 flex flex-wrap gap-4 tablet:gap-24">
               <div class="text-white w-fit tablet:w-fit largeScreen:w-fit font-maven-medium">
                 <p class="text-6px tablet:text-10px largeScreen:text-12px">Mail</p>
                 <p class="-mt-1 tablet:-mt-2 text-8px tablet:text-base largeScreen:text-lg">yannishaismann@outlook.fr</p>
@@ -55,14 +55,14 @@ export default defineComponent({
           </div>
           <div>
             <p class="text-white font-maven-medium text-12px tablet:text-20px">Public informations</p>
-            <div class="mt-1 tablet:mt-4 flex gap-4 tablet:gap-24">
+            <div class="mt-1 tablet:mt-4 flex flex-wrap gap-10 tablet:gap-24">
               <div class="text-white w-fit tablet:w-fit largeScreen:w-fit font-maven-medium">
                 <p class="text-6px tablet:text-10px largeScreen:text-12px">Pseudo</p>
                 <p class="-mt-1 tablet:-mt-2 text-8px tablet:text-base largeScreen:text-lg">yesmanGaming</p>
               </div>
               <div class="text-white w-fit tablet:w-fit largeScreen:w-fit font-maven-medium">
                 <p class="text-6px tablet:text-10px largeScreen:text-12px">Image</p>
-                <img class="tablet:w-12" src="./../../assets/default-pdp.png"/>
+                <img class="w-6 tablet:w-12" src="./../../assets/default-pdp.png"/>
               </div>
             </div>
           </div>
