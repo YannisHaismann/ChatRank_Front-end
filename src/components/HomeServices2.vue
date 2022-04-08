9f9b75f5f5ef3094e1eca271d77ee0a46565ee26
Yannis Haismann <72089971+YannisHaismann@users.noreply.github.com>
2022-04-08 16:43:04 +0200

    fix: service 2 position

--- src/components/HomeServices2.vue
+++ src/components/HomeServices2.vue
@@ -14,13 +14,13 @@ export default defineComponent({
 
 <template>
   <div class="w-full h-screen flex flex-col lg:flex-row p-20" id="service2">
-    <div class="relative h-1/2 lg:h-full lg:w-1/2 order-1 lg:order-2">
+    <div class="relative h-1/2 lg:h-full lg:w-1/2 order-1 lg:order-1">
       <SlimCircleService class="right-0" />
       <div class="relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 w-25rem md:w-36rem xl:w-44rem 2xl:w-50rem h-full">
         <img class="relative top-1/2 transform -translate-y-1/2 w-full" src="./../assets/service_2.png" alt="">
       </div>
     </div>
-    <div class="text-white font-maven-medium lg:w-1/2 lg:pl-40 2xl:pl-60 my-auto">
+    <div class="text-white font-maven-medium lg:w-1/2 lg:pl-40 2xl:pl-60 my-auto lg:order-2">
       <h2 class="font-maven-black text-18px md:text-20px mb-4">Loremp</h2>
       <p class="text-12px md:text-18px ml-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam totam molestias voluptatibus quis nisi, veniam aliquid optio sequi accusamus aspernatur! Quaerat inventore, ipsum debitis eos ipsa impedit neque consequatur sit.</p>
     </div>
