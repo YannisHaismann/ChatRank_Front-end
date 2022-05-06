426b892c6a76c72d974ffb9bd00b9d459aa3c5ec
Yannis Haismann <72089971+YannisHaismann@users.noreply.github.com>
2022-05-06 12:20:49 +0200

    feat: correction size link back home

--- src/components/BackHomeBtn.vue
+++ src/components/BackHomeBtn.vue
@@ -13,9 +13,9 @@ export default defineComponent({
 </script>
 
 <template>
-<router-link to="/">
-  <div class="flex" id="BackHomeBtn">
-    <img class="w-8 sm:w-10" :src="backhome">
+<router-link class="h-fit" to="/">
+  <div class="flex h-14 sm:h-16" id="BackHomeBtn">
+    <img class="w-8 h-8 sm:h-10 sm:w-10" :src="backhome">
     <p class="font-maven-medium ml-3 mt-0.5 sm:mt-1 text-white text-20px sm:text-22px">Back home</p>
   </div>
 </router-link>
