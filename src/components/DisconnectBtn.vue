545c667a384cc521a2d8d230547cbfac850496d7
Yannis Haismann <72089971+YannisHaismann@users.noreply.github.com>
2022-05-15 09:31:53 +0200

    feat: disconnect btn

---
+++ src/components/DisconnectBtn.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import disconnect from "@/assets/disconnect.svg";
+
+export default defineComponent({
+  name: "DisconnectBtn",
+  props: { },
+  components: { },
+  setup() {
+    const disconnectF = () => {
+      localStorage.removeItem('back_token');
+      localStorage.removeItem('twitch_access_token');
+      localStorage.removeItem('back_refresh_token');
+      window.location.href = '/';
+      // window.location.reload();
+    }
+    return { disconnect, disconnectF };
+  },
+});
+</script>
+
+<template>
+  <div @click="disconnectF" class="flex h-14 sm:h-16 cursor-pointer" id="DisconnectBtn">
+    <p class="font-maven-medium mt-0.5 sm:mt-1 text-white text-20px sm:text-22px">Disconnect</p>
+    <img class="w-8 h-8 sm:h-10 ml-3 sm:w-10" :src="disconnect">
+  </div>
+</template>
--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -2,6 +2,7 @@
 import { defineComponent, onMounted } from "vue";
 import Dashboard from "@/components/Dashboard.vue";
 import BackHomeBtn from "@/components/BackHomeBtn.vue"
+import DisconnectBtn from "@/components/DisconnectBtn.vue"
 import { useStore } from "vuex";
 
 export default defineComponent({
@@ -9,6 +10,7 @@ export default defineComponent({
   components: {
     Dashboard,
     BackHomeBtn,
+    DisconnectBtn,
   },
   setup() {
     const store = useStore();
@@ -26,7 +28,10 @@ export default defineComponent({
 <template>
   <div id="MyAccount" class="w-full h-full flex bg-gradient-to-tr from-bgLeftColor via-bgMiddleColor to-bgDarkGreen relative z-10">
     <div class="m-auto flex flex-col w-2/3 h-3/4">
-      <BackHomeBtn class="ml-10 -mb-5" />
+      <div class="flex justify-between">
+        <BackHomeBtn class="ml-10 -mb-10" />
+        <DisconnectBtn class="mr-10 -mb-10" />
+      </div>
       <Dashboard />
       <img class="w-40 sm:w-96 absolute right-1 top-14 sm:top-0 sm:right-10 z-n1" src="./../assets/Cube-1.png" alt="">
       <img class="w-40 sm:w-96 absolute right-0 top-24 sm:right-2 sm:top-24 z-n1" src="./../assets/RoundCube-2.png" alt="">
